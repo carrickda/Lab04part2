042f1f5fc9d90ff41fcbd4eb4b673587a7aed360
carrickda <carrickda@gmail.com>
2022-09-27 02:29:36 +0000

    added js file

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console. log("Hello World!");
+
+var LeaguePlayer = prompt('Wat is your name?');
+alert('Hello,  ' + LeaguePlayer + ' Click OK to proceed');
+
+
+var Champion = prompt('Do you play League?');
+
+if (Champion == 'no'){
+    alert('You should play it is fun. Click OK to proceed.');
+} else {
+    prompt('Which chanmp do you play?');
+} 
+    
+
+alert('I play Support and Jungle. My main is Zyra support and Vi jungle');
